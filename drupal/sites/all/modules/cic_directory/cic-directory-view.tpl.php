679477b2dd136bf785d9391f8c21ad964a52697b
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-05-09 12:50:00 +0100

    third draft

--- drupal/sites/all/modules/cic_directory/cic-directory-view.tpl.php
+++ drupal/sites/all/modules/cic_directory/cic-directory-view.tpl.php
@@ -24,17 +24,16 @@
 
 <div class='quarter qb ql' id='q3'>
 	<h3>Maps and links</h3>
- 	<div id="map_container"><div id="map_canvas"></div></div>
-	<div><?php print $links['map_link']; ?></div>
-	<div><?php print $links['ons']; ?></div>
-	<div><?php print $links['stats']; ?></div>
-</div>
+	<?php print cic_directory_theme_var('', $links['map_link'], 'image', $links['map_link_image']); ?>	
+	<?php print cic_directory_theme_var('', $links['ons'], 'image', $links['ons_image']); ?>	
+	<?php print cic_directory_theme_var('', $links['stats'], 'image', $links['stats_image']); ?>	
+	</div>
 
 <div class='quarter qb qr' id='q4'>
 	<h3>Documents</h3>
-	<p><a href="<?php print $docs['1']; ?>">Document 1</a></p>
-	<p><a href="<?php print $docs['2']; ?>">Document 2</a></p>
-	<p><a href="<?php print $docs['3']; ?>">Document 3</a></p>
+	<?php print cic_directory_theme_var('', $docs['1']); ?>	
+	<?php print cic_directory_theme_var('', $docs['2']); ?>	
+	<?php print cic_directory_theme_var('', $docs['3']); ?>	
 </div>
 </div>
 <div id='cross_footer'></div>
