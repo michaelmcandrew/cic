889fe405bcd76d8295723d1624d4c6763e02a624
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-05-09 20:09:06 +0100

    better text for no search results

--- drupal/sites/all/modules/cic_directory/cic-directory-search.tpl.php
+++ drupal/sites/all/modules/cic_directory/cic-directory-search.tpl.php
@@ -9,7 +9,7 @@
 <?php if(isset($results)): ?>
 
 	<div id='results'>
-	<p>The following Churches match your query.</p>
+	<p>The following Churches match your query:</p>
 	
 	<div id='table'>
 		<?php echo $results; ?>
@@ -21,9 +21,16 @@
 			<?php echo $pager; ?>
 		</div>
 		
+
 	<?php endif; ?>
 
 	</div>
-
+<?php else: ?>
+<p><i>Sorry, your search did not match any churches or projects.</i></p>
+<p>Suggestions:</p>
+<ul>
+	<li>Try fewer search words and criteria</li>
+	<li>Make sure search words are spelt correctly</li>
+</ul>
 <?php endif; ?>
 
