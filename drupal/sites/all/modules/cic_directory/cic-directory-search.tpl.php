3cb67a019a7ac4b297ed2adfddff22ec4a018274
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2011-05-20 13:57:40 +0100

    smaller orb, cached pages working, link to request form, added ward, added icon

--- drupal/sites/all/modules/cic_directory/cic-directory-search.tpl.php
+++ drupal/sites/all/modules/cic_directory/cic-directory-search.tpl.php
@@ -38,7 +38,7 @@
 
 <?php else: ?>
 	<p>Welcome to Commitment in Communities’ (CiC) database. This facility has been provided to enable easy access to a range of information on Methodist Churches and projects, and ecumenical and partnership working with other faiths and organisations.</p>
-	<p>The information available is as current as we can make it but in the event that you have more up-to-date information on your church or project, please let us know by <a href="http://www.c-i-c.org.uk/contact.html">contacting CiC</a>. Also, as feedback is very important to us, please use the <a href="http://www.c-i-c.org.uk/contact.html">comment form on our website</a> to let us know how the facility is performing or to provide suggestions on how to improve it.</p>
+	<p>The information available is as current as we can make it but in the event that you have more up-to-date information on your church or project, please let us know by <a href="http://www.c-i-c.org.uk/contact.html">contacting CiC</a>. Also, as feedback is very important to us, please use the <a href="http://www.c-i-c.org.uk/request.html">comment form on our website</a> to let us know how the facility is performing or to provide suggestions on how to improve it.</p>
 	<?php echo $form; ?>
 	
 	<!-- add any intro text here if desired -->
